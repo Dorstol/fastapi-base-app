94c0df6ead3502e38bfb0a41de85a4ea619dd8a1
Dorstol <olehsofronov@gmail.com>
2024-05-27 14:20:52 +0300

    create database config

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from pydantic import BaseModel
+from pydantic import PostgresDsn
 
 
 class RunConfig(BaseModel):
@@ -11,9 +12,22 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    host: str = "localhost"
+    port: int = 5432
+    user: str = "user"
+    password: str = "password"
+    database: str = "shop"
+
+    @property
+    def url(self):
+        return f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig = DatabaseConfig()
 
 
 settings = Settings()
